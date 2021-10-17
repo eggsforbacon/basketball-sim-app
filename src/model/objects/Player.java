eac6fe7a7a5512f91d18093bc8df2337a040b82d
Samuel Hernandez <doicirohangnan@gmail.com>
2021-10-16 23:29:34 -0500

    Created player and team constructor

--- src/model/objects/Player.java
+++ src/model/objects/Player.java
@@ -20,18 +20,18 @@ public class Player implements Serializable {
     private int goalsAttempted; //The amount of times player THREW the ball (GA)
     private int goalsScored; //The amount of THROWS which resulted in POINTS (GS) <- MUST BE LOWER THAN GA
     private int freeThrowsAttempted; //(FTA) <- MUST BE AT MOST HALF OF GA, AND ALWAYS LOWER THAN GS
-    private int turnovers; //(TOV)
-    private int minutesPLayed; //(MP)
+    private double turnovers; //(TOV)
+    private double minutesPLayed; //(MP)
     private int assists; //(AST) <- LOWER THAN GS
-    private int offensiveRebounds; //(ORB)
-    private int defensiveRebounds; //(DRB)
-    private double deffensiveBPM; //(DBPM) <- WITHIN 10 AND -10
+    private double offensiveRebounds; //(ORB)
+    private double defensiveRebounds; //(DRB)
+    private double defensiveBPM; //(DBPM) <- WITHIN 10 AND -10
     private double offensiveBPM; //(OBPM) <- WITHIN 10 AND -10
 
     //Sample raw data string:
     // NAME; AGE; NUM; POS; ACTIVE; 2PTS; 3PTS; GA; GS; FTA; TOV; MP; AST; ORB; DRB; DBPM; OBPM; TM NAME; TM PLAYERS NUM; TM MP; TM GA; TM GS; TM FTA; TM TOV; TM TRB; Opponent Rebounds
 
-    private int totalRebounds; // = offensiveRebounds + defensiveRebounds (TRB)
+    private double totalRebounds; // = offensiveRebounds + defensiveRebounds (TRB)
     private final double BIAS = 0.44;
 
     //Calculated Stats
@@ -43,5 +43,36 @@ public class Player implements Serializable {
 
     public Player(String name) {
         this.name = name;
+    }
+
+    public Player(String name, int age, int number, String position, boolean active,
+                  int points, int goalsAttempted, int goalsScored, int freeThrowsAttempted,
+                  double turnovers, double minutesPLayed, int assists, double offensiveRebounds,
+                  double defensiveRebounds, double defensiveBPM, double offensiveBPM, String teamName,
+                  int teamPlayerNumber, double teamMinutePlayed, int teamGoalsAttempted, int teamGoalsScored,
+                  int teamFreeThrowsAttempted, double teamTurnovers, double teamRebounds, double opponentRebounds) {
+        this.name = name;
+        this.age = age;
+        this.number = number;
+        this.position = position;
+        this.active = active;
+        this.points = points;
+        this.goalsAttempted = goalsAttempted;
+        this.goalsScored = goalsScored;
+        this.freeThrowsAttempted = freeThrowsAttempted;
+        this.turnovers = turnovers;
+        this.minutesPLayed = minutesPLayed;
+        this.assists = assists;
+        this.offensiveRebounds = offensiveRebounds;
+        this.defensiveRebounds = defensiveRebounds;
+        totalRebounds = offensiveRebounds + defensiveRebounds;
+        this.defensiveBPM = defensiveBPM;
+        this.offensiveBPM = offensiveBPM;
+        team = new Team(teamName, teamPlayerNumber, teamMinutePlayed, teamGoalsAttempted, teamGoalsScored, teamFreeThrowsAttempted, teamTurnovers, teamRebounds);
+        calculateReboundPercentage(opponentRebounds);
+    }
+
+    private void calculateReboundPercentage(double opponentRebounds) {
+
     }
 }
--- src/model/objects/Team.java
+++ src/model/objects/Team.java
@@ -5,8 +5,27 @@ import java.io.Serializable;
 public class Team implements Serializable {
 
     private String name;
+    private int players;
+    private double minutesPlayed;
+    private int goalsAttempted;
+    private int goalsScored;
+    private int freeThrowsAttempted;
+    private double turnovers;
+    private double rebounds;
 
     public Team(String name) {
         this.name = name;
+    }
+
+    public Team(String name, int players, double minutesPlayed, int goalsAttempted, int goalsScored,
+                int freeThrowsAttempted, double turnovers, double rebounds) {
+        this.name = name;
+        this.players = players;
+        this.minutesPlayed = minutesPlayed;
+        this.goalsAttempted = goalsAttempted;
+        this.goalsScored = goalsScored;
+        this.freeThrowsAttempted = freeThrowsAttempted;
+        this.turnovers = turnovers;
+        this.rebounds = rebounds;
     }
 }
