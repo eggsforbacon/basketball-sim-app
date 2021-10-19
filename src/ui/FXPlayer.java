17b2f6ea17907062a32454bd4a78d9177e15460c
David Montaño Tamayo <cuatro04sr@gmail.com>
2021-10-18 19:11:29 -0500

    Started with players GUI

--- src/ui/FXPlayer.java
+++ src/ui/FXPlayer.java
@@ -1,14 +1,37 @@
 package ui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import model.objects.Fiba;
 
 public class FXPlayer {
-    
+
+    private static final long serialVersionUID = 1;
     private Fiba fb;
     private FXController xGui;
-    
+
     public FXPlayer(Fiba fb, FXController xGui) {
         this.fb = fb;
         this.xGui = xGui;
+    }
+
+    @FXML
+    void onNextV(ActionEvent event) {
+        try {
+            changeTextFieldsSelecteds(rc.findVehicletoShowNext(positionCar));
+            positionCar++;
+        } catch (NullPointerException e) {
+            fxGUI.showAlert(false, "No hay vehiculos para mostrar", stackPane);
+        }
+    }
+
+    @FXML
+    void onPrevV(ActionEvent event) {
+        try {
+            changeTextFieldsSelecteds(rc.findVehicletoShowPrev(Math.abs(amountCar)));
+            amountCar--;
+        } catch (NullPointerException e) {
+            fxGUI.showAlert(false, "No hay vehiculos para mostrar", stackPane);
+        }
     }
 }
