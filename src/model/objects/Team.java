1773f70fb9150602bcfe2610d8e36ad4b6568588
Samuel Hernandez <doicirohangnan@gmail.com>
2021-10-20 23:56:19 -0500

    Removed unnecessary variables

--- src/model/objects/Team.java
+++ src/model/objects/Team.java
@@ -5,31 +5,16 @@ import java.io.Serializable;
 public class Team implements Serializable {
 
     private String name;
+    private String country;
     private int players;
-    private double minutesPlayed;
-    private int goalsAttempted;
-    private int goalsScored;
-    private int freeThrowsAttempted;
-    private double turnovers;
-    private double rebounds;
-
-    private double totalShootingAttempts; //GA + BIAS * FTA (TSA)
 
     public Team(String name) {
         this.name = name;
     }
 
-    public Team(String name, int players, double minutesPlayed, int goalsAttempted, int goalsScored,
-                int freeThrowsAttempted, double turnovers, double rebounds) {
+    public Team(String name, String country) {
         this.name = name;
-        this.players = players;
-        this.minutesPlayed = minutesPlayed;
-        this.goalsAttempted = goalsAttempted;
-        this.goalsScored = goalsScored;
-        this.freeThrowsAttempted = freeThrowsAttempted;
-        this.turnovers = turnovers;
-        this.rebounds = rebounds;
-        totalShootingAttempts = goalsAttempted + 0.44 * freeThrowsAttempted;
+        this.country = country;
     }
 
     public String getName() {
@@ -48,59 +33,19 @@ public class Team implements Serializable {
         this.players = players;
     }
 
-    public double getMinutesPlayed() {
-        return minutesPlayed;
+    public String getCountry() {
+        return country;
     }
 
-    public void setMinutesPlayed(double minutesPlayed) {
-        this.minutesPlayed = minutesPlayed;
+    public void setCountry(String country) {
+        this.country = country;
     }
 
-    public int getGoalsAttempted() {
-        return goalsAttempted;
+    public void addPlayer() {
+        players++;
     }
 
-    public void setGoalsAttempted(int goalsAttempted) {
-        this.goalsAttempted = goalsAttempted;
-    }
-
-    public int getGoalsScored() {
-        return goalsScored;
-    }
-
-    public void setGoalsScored(int goalsScored) {
-        this.goalsScored = goalsScored;
-    }
-
-    public int getFreeThrowsAttempted() {
-        return freeThrowsAttempted;
-    }
-
-    public void setFreeThrowsAttempted(int freeThrowsAttempted) {
-        this.freeThrowsAttempted = freeThrowsAttempted;
-    }
-
-    public double getTurnovers() {
-        return turnovers;
-    }
-
-    public void setTurnovers(double turnovers) {
-        this.turnovers = turnovers;
-    }
-
-    public double getRebounds() {
-        return rebounds;
-    }
-
-    public void setRebounds(double rebounds) {
-        this.rebounds = rebounds;
-    }
-
-    public double getTotalShootingAttempts() {
-        return totalShootingAttempts;
-    }
-
-    public void setTotalShootingAttempts(double totalShootingAttempts) {
-        this.totalShootingAttempts = totalShootingAttempts;
+    public void removePlayer() {
+        players--;
     }
 }
